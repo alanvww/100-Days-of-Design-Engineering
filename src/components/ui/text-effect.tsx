847832eb74b6c2000072ce875515b7364501d3a6
Alan Ren <10086000+alanvww@users.noreply.github.com>
2025-02-20 14:51:56 -0500

    Fix shacdn with TailwindCSS v4

--- src/components/ui/text-effect.tsx
+++ src/components/ui/text-effect.tsx
@@ -10,7 +10,7 @@ import {
 } from 'motion/react';
 import React from 'react';
 
-export type PresetType = 'blur-sm' | 'fade-in-blur' | 'scale' | 'fade' | 'slide';
+export type PresetType = 'blur' | 'fade-in-blur' | 'scale' | 'fade' | 'slide';
 
 export type PerType = 'word' | 'char' | 'line';
 
@@ -234,12 +234,12 @@ export function TextEffect({
 
   const customStagger = hasTransition(variants?.container?.visible ?? {})
     ? (variants?.container?.visible as TargetAndTransition).transition
-        ?.staggerChildren
+      ?.staggerChildren
     : undefined;
 
   const customDelay = hasTransition(variants?.container?.visible ?? {})
     ? (variants?.container?.visible as TargetAndTransition).transition
-        ?.delayChildren
+      ?.delayChildren
     : undefined;
 
   const computedVariants = {
