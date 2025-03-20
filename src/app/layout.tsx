1b50b5892ef34c0cf40c8fabc918385333513d8b
Alan Ren <10086000+alanvww@users.noreply.github.com>
2025-03-19 23:54:32 -0400

    Update favicon implementation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,14 @@ export const metadata: Metadata = {
     description: "Showcase of Alan's design engineering journey over 100 days.",
     creator: "@alanvww",
   },
+  manifest: '/site.webmanifest',
+  icons: {
+    apple: '/apple-touch-icon.png',
+    icon: [
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' }
+    ]
+  },
 };
 
 export default function RootLayout({
@@ -39,10 +47,10 @@ export default function RootLayout({
     <ViewTransitions>
       <html lang="en">
         <head>
-          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png" />
+          <link rel="manifest" href="site.webmanifest" />
           <meta name="theme-color" content="#000000" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         </head>
